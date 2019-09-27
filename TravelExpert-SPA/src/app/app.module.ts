c0e01ca0297edc060107102329709fcd565adcea
835266 <835266@ICT218460>
2019-09-27 09:34:36 -0600

    Got rid of test Value Class

--- TravelExpert-SPA/src/app/app.module.ts
+++ TravelExpert-SPA/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { CustomerComponent } from './customer/customer.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       CustomerComponent
    ],
    imports: [
--- TravelExperts.API/Startup.cs
+++ TravelExperts.API/Startup.cs
@@ -27,7 +27,6 @@ namespace TravelExperts.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("TravelExpertsDBConnection")));
             services.AddDbContext<TravelExpertsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TravelExpertsDBConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
