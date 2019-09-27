e051dd6c81bc10956f7dde1e39f69afe262d46cf
835266 <835266@ICT218460>
2019-09-27 17:38:46 -0600

    Implemented routes. Need guards

--- TravelExpert-SPA/src/app/navbar/navbar.component.ts
+++ TravelExpert-SPA/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavbarComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
     this.loadScript('../assets/scripts/navbar.component.js');
@@ -20,6 +22,7 @@ export class NavbarComponent implements OnInit {
     this.authService.login(this.model).subscribe(next =>{
 
       this.alertify.success('Logged in successfully');
+      this.router.navigate(['/bookedPackages']);
     }, error =>{
       this.alertify.error(error);
     });
@@ -33,6 +36,7 @@ export class NavbarComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');
     this.alertify.message('Logged out.');
+    this.router.navigate(['/home']);
   }
 
   public loadScript(url: string) {
