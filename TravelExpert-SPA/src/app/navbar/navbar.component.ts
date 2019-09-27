16deb7842efc3f5ff9fc54633f5b15f08912a60b
835266 <835266@ICT218460>
2019-09-27 13:44:34 -0600

    fixed nav css

--- TravelExpert-SPA/src/app/navbar/navbar.component.ts
+++ TravelExpert-SPA/src/app/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent implements OnInit {
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.loadScript('../assets/scripts/navbar.component.js');
   }
 
   login(){
@@ -21,6 +22,16 @@ export class NavbarComponent implements OnInit {
       console.log('Failed to login');
     });
 
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
   }
 
 
