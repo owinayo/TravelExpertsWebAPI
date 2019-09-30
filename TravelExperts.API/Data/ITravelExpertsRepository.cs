134301e53909f74ea6ddfdc6cb87e2ae1a757501
835266 <835266@ICT218460>
2019-09-30 09:32:03 -0600

    writing db Code

---
+++ TravelExperts.API/Data/ITravelExpertsRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using TravelExperts.API.Model;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TravelExperts.API.Data
+{
+    public interface ITravelExpertsRepository
+    {
+        void Add<T>(T entity) where T: class;
+
+        void Delete<T>(T entity) where T: class;
+
+        Task<bool> SaveAll();
+
+        Task<Customers> GetCustomer(int id);
+
+        Task<Customers> EditCustomer(Customers oldCustomer, Customers newCustomer); 
+
+        Task<IEnumerable<Packages>> GetAllAvailablePackages();
+
+        Task<IEnumerable<Packages>> GetBookedPackagesByCustomer(int customerId);
+
+        
+    }
+}
---
+++ TravelExperts.API/Data/TravelExpertsRepository.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TravelExperts.API.Model;
+
+namespace TravelExperts.API.Data
+{
+    public class TravelExpertsRepository : ITravelExpertsRepository
+    {
+        private TravelExpertsContext context;
+
+        public TravelExpertsRepository(TravelExpertsContext context){
+            this.context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            context.Remove(entity);            
+        }
+
+        public async Task<Customers> EditCustomer(Customers oldCustomer, Customers newCustomer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Packages>> GetAllAvailablePackages()
+        {
+            var packages = await context.Packages.ToListAsync();
+            return packages;
+        }
+
+        public async Task<IEnumerable<Packages>> GetBookedPackagesByCustomer(int customerId)
+        {
+            var packages = 
+            (from c in context.Customers
+            join b in context.Bookings on c.CustomerId equals b.CustomerId
+            join p in context.Packages on b.PackageId equals p.PackageId
+            where c.CustomerId == customerId
+            select new {p.PackageId, p.PkgName, p.Image, p.Partner, p.AirfairInclusion,
+            p.PkgStartDate, p.PkgEndDate, p.PkgDesc, p.PkgBasePrice, p.PkgAgencyCommission});
+
+            return await packages.Cast<Packages>().ToListAsync();
+        }
+
+        public async Task<Customers> GetCustomer(int id)
+        {
+            var customer = await this.context.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+            return customer;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
